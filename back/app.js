036d60597345ec8677543a73831cb293cd3c7eef
a18marcastru <a18marcastru@inspedralbes.cat>
2024-09-25 13:29:49 +0200

    Projecta 0

---
+++ back/app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const port = 3000;
+
+app.use(cors());
+
+app.get('/start', (req, res) => {
+  fs.readFile('preguntes.json', 'utf8', (err, data) => {
+    if (err) {
+      // Si hay un error al leer el archivo, envía una respuesta de error
+      return res.status(500).json({ message: 'Error al leer el archivo JSON' });
+    }
+
+    // Convierte el contenido del archivo JSON a objeto JavaScript
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/over', (req, res) => {
+  //const newObj = aleatorio(obj);
+  const pathDir = __dirname + "\\Jocs";
+  
+  const fechaActual = new Date();
+  const dia = fechaActual.getDate();
+  const mes = fechaActual.getMonth() + 1;
+  const year = fechaActual.getFullYear(); 
+  const pathDirective = path.join(pathDir, `${dia}-${mes}-${year}`);
+
+  if(!fs.existsSync(pathDirective)) {
+    fs.mkdir(pathDirective, (err) => {
+      if(err) return res.send("Error");
+      else return res.send("Creada");
+    });
+  }
+  else res.send("Ya existe");
+
+  // fs.writeFile(pathFile, obj, (err) => {
+  //   if(err) return res.status(500).json({mesaje: 'Error al crear archivo'})
+  //   res.status(200).json({mensaje: 'Archivo creado'})
+  // });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// function random(jsonData) {
+
+//   // let numQ = [];
+//   questions2 = jsonData.preguntes;
+
+//   const newJson = questions2.map(q => ({
+//     pregunta: q.pregunta,
+//     respostes: mezclarRespuestas(q.resposta_correcta, q.respostes_incorrectes),
+//     imatge: q.imatge
+//   }));
+
+//   // for(let i = 0;i < questions2.length;i++) {
+//   //     numQ[i] = questions[Math.floor(Math.random() * questions.length)];
+//   // }
+
+//   // const newJson = numQ.map(q => ({
+//   //     pregunta: q.pregunta,
+//   //     respostes: mezclarRespuestas(q.resposta_correcta, q.respostes_incorrectes),
+//   //     imatge: q.imatge
+//   // }));
+
+//   return newJson;
+// }
+
+// function mezclarRespuestas(resposta_correcta, respostes_incorrectes) {
+//   const respuestas = [resposta_correcta, ...respostes_incorrectes];
+//   return respuestas.sort(() => Math.random() - 0.5); // Mezcla aleatoria de respuestas
+// }
---
+++ web/vue-autoescola/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+
+
+let newObj = ref([]);
+
+onMounted(() => {
+  fetch('http://localhost:3000/start').then(res => res.json())
+  .then((data) => {
+    newObj.value = data.preguntes;
+  })
+})
+
+// function mezclarRespuestas(pregunta) {
+//   const respuestas = [pregunta.resposta_correcta, ...pregunta.respostes_incorrectes];
+//   return respuestas.sort(() => Math.random() - 0.5); // Mezcla aleatoria de respuestas
+// }
+
+</script>
+
+<template>
+  <header>
+    <h1>Autoescola</h1>
+  </header>
+  <br>
+
+  <main>
+    <div id="preguntes" v-for="(preguntes, index) in newObj" :key="index">
+      <h2>{{ preguntes.pregunta }}</h2>
+      <ul>
+        <li v-for="(respuesta, i) in mezclarRespuestas(preguntes)" :key="i">
+          {{ respuesta }}
+        </li>
+      </ul>
+      <button>Eliminar</button><button>Acutalizar</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
